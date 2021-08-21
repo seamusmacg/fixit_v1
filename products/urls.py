5a15ebd48dd3873bda956c2ce0348c382d1bd6ef
James <seamus.macgrianna12@gmail.com>
2021-08-21 18:58:55 +0100

    Add product page items

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_products, name='products')
+    path('', views.get_products, name='products'),
+    path('<product_id>', views.get_product, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def get_products(request):
-    """A view that returns product page with product data.
+    """A view that returns products page with all product data.
 
     Args:
         request (GET): HTTP request for products.html
@@ -19,3 +19,21 @@ def get_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def get_product(request, product_id):
+    """A view that returns product page with product data.
+
+    Args:
+        request (GET): HTTP request for products.html
+
+    Returns:
+        HTML file: Products HTML file 
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
