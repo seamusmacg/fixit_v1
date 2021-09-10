92d9867b1dbf1c6afae650e06ae312b6a7100f6e
James <seamus.macgrianna12@gmail.com>
2021-09-10 23:40:23 +0100

    Create contact form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages 
 # Create your views here.
 
 def index(request):
@@ -13,3 +13,27 @@ def index(request):
     """
     
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """View that returns contact page
+
+    Args:
+        request (GET): HTTP request for contact.html
+
+    Returns:
+        HTML file: contact HTML file 
+    """
+
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        question = request.POST['question']
+        if name == "" or email == "" or question == "":
+            messages.warning(request, "There is one or more empty fields - cannot be empty!")
+        else:
+            return render(request, 'home/success.html')
+
+  
+    return render(request, 'home/contact.html')
+
