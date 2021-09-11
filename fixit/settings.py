1b69201390b376b0ddf05863834138ca3097178a
James <seamus.macgrianna12@gmail.com>
2021-09-11 17:39:03 +0100

    Modified secret key and set debug

--- fixit/settings.py
+++ fixit/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wei%wbu4##ys7i@_^-!py&h+oa%(in=-r$tw(j+=6n6kxzo$hn'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fix-it.herokuapp.com', 'localhost']
 
