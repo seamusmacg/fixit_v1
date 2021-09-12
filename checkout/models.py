0c8e68494b14510476e5753ef6d5f615729e9f00
James <seamus.macgrianna12@gmail.com>
2021-09-12 01:45:25 +0100

    Change length of order fields

--- checkout/models.py
+++ checkout/models.py
@@ -13,15 +13,14 @@ from profiles.models import Profile
 
 class Order(models.Model):
     """Order model"""
-    
-    order_number = models.TextField(max_length=255, null=False, editable=False)
+    order_number = models.CharField(max_length=255, null=False, editable=False)
     user_profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
-    name = models.TextField(max_length=255, null=False, blank=False)
-    email = models.EmailField(max_length=255, null=False, blank=False)
-    phone = models.TextField(max_length=255, null=False, blank=False)
-    postcode = models.TextField(max_length=255, null=True, blank=True)
-    town = models.TextField(max_length=255, null=False, blank=False)
-    address = models.TextField(max_length=254, null=False, blank=False)
+    name = models.CharField(max_length=60, null=False, blank=False)
+    email = models.EmailField(max_length=350, null=False, blank=False)
+    phone = models.CharField(max_length=18, null=False, blank=False)
+    postcode = models.CharField(max_length=15, null=True, blank=True)
+    town = models.CharField(max_length=50, null=False, blank=False)
+    address = models.CharField(max_length=100, null=False, blank=False)
     country = CountryField(blank_label='Country *', null=False, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
