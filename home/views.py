9bbdae3ed2b97fb227cc32c2b6c43c8145cfb461
James <seamus.macgrianna12@gmail.com>
2021-09-12 11:48:23 +0100

    Add Xframe exemption

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages 
+from django.views.decorators.clickjacking import xframe_options_exempt
 # Create your views here.
 
+
+@xframe_options_exempt
 def index(request):
     """View that returns index page
 
