ab6dfd36cff31fefe238a579b6adc4d440ef538f
James <seamus.macgrianna12@gmail.com>
2021-10-26 18:43:49 +0100

    Add Category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, Category
 
 # Register your models here.
 admin.site.register(BlogPost)
+admin.site.register(Category)
 
 
--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,22 @@ from django.urls import reverse
 from datetime import datetime, date
 
 #Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.name
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
    
     def __str__(self):
         return self.title + ' | ' + str(self.author)
