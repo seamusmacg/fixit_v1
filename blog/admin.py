566afe3e35c891b527b6f581a831ce585b2d0b42
James <seamus.macgrianna12@gmail.com>
2021-10-26 18:57:25 +0100

    Remove Category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import BlogPost, Category
+from .models import BlogPost
 
 # Register your models here.
 admin.site.register(BlogPost)
-admin.site.register(Category)
+
 
 
