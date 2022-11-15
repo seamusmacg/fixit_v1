7db074968e760217f996452153903dead0e86553
James <seamus.macgrianna12@gmail.com>
2022-11-15 20:39:56 +0000

    Change DEBUG mode to prod

--- fixit/settings.py
+++ fixit/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get(
     'SECRET_KEY', '')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['fix-it.herokuapp.com', '127.0.0.1']
 
