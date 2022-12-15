be43355142c0f473c2decb8fc69cc721dc2f94d2
James <seamus.macgrianna12@gmail.com>
2022-12-15 15:41:34 +0000

    Change debug mode

--- fixit/settings.py
+++ fixit/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['fix-it.herokuapp.com', '127.0.0.1']
